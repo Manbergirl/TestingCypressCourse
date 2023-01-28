eb4c8f4f5a440f98fa0adbae58b606cd4b5ea683
Beatris Dimitrova <Beatris.Dimitrovaaa@gmail.com>
2023-01-28 15:16:24 +0200

    test: subscribe list does NOT allow an invalid email address

--- cypress/e2e/subscribe.cy.ts
+++ cypress/e2e/subscribe.cy.ts
@@ -1,20 +1,23 @@
 describe("newsLetter Subscribe Form", () => {
     beforeEach(() =>{
     cy.visit('http://localhost:3000/')
- })
+    })
 
-it("allows user to subscribe to the email list",() => {
+    it("allows user to subscribe to the email list",() => {
 
     cy.getByData("email-input").type("Beatris.Dimitrovaaa@gmail.com")
     cy.getByData("submit-button").click();
     cy.getByData("success-message").should("exist").contains("Beatris.Dimitrovaaa@gmail.com") 
-})
+    })
 
-// it("does NOT allow a invalid email address",()=>{
+    it.only("does NOT allow an invalid email address",()=>{
+    
+    cy.getByData("email-input").type("Bea")
+    cy.getByData("submit-button").click();
+    cy.getByData("success-message").should("not.exist")
 
 
-
-// })
+    })
 
 
 }) 
