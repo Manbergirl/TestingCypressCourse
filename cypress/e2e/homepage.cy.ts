f6f8476a7ed6c1f55856f8a4595947d716eb9d74
Beatris Dimitrova <Beatris.Dimitrovaaa@gmail.com>
2023-01-28 16:24:52 +0200

    test: Testing your first nextjs application

--- cypress/e2e/homepage.cy.ts
+++ cypress/e2e/homepage.cy.ts
@@ -4,13 +4,13 @@
 
   })
   context("hero section",()=>{
-  it('the h1 contains the correct text ', () => {
+    it('the h1 contains the correct text ', () => {
     cy.getByData("hero-heading").contains(
       "Testing Next.js Applications with Cypress"
     )
   })
 
-  it('the features on the homepage are correct', () => {
+    it('the features on the homepage are correct', () => {
     cy.get('dt').eq(0).contains('4 Courses')
     cy.get('dt').eq(1).contains('25+ Lessons')
     cy.get('dt').eq(2).contains('Free and Open Source')
@@ -18,4 +18,17 @@
 
 })
 
+  context("course section",()=>{
+    it.only("course: Testing your first next.js application",()=>{
+    cy.getByData("course-0").find("a").eq(3).click()
+    cy.location("pathname").should("eq","/testing-your-first-application")
+
+})
+
+
+
+
+
+
+})
 })
