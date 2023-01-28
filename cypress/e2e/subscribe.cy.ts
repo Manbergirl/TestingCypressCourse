af141a804ed630e042c7108b607b2330b8eba0ab
Beatris Dimitrova <Beatris.Dimitrovaaa@gmail.com>
2023-01-28 15:31:46 +0200

    test:user cannot subscribe with already existing email address

--- cypress/e2e/subscribe.cy.ts
+++ cypress/e2e/subscribe.cy.ts
@@ -10,13 +10,19 @@ describe("newsLetter Subscribe Form", () => {
     cy.getByData("success-message").should("exist").contains("Beatris.Dimitrovaaa@gmail.com") 
     })
 
-    it.only("does NOT allow an invalid email address",()=>{
+    it("does NOT allow an invalid email address",()=>{
     
     cy.getByData("email-input").type("Bea")
     cy.getByData("submit-button").click();
     cy.getByData("success-message").should("not.exist")
+    })
 
+    it.only("user cannot subscribe with already existing email address",()=>{
 
+    cy.getByData("email-input").type("john@example.com")
+    cy.getByData("submit-button").click();
+    cy.getByData("server-error-message").should("exist").
+    contains("Error: john@example.com already exists. Please use a different email address")
     })
 
 
